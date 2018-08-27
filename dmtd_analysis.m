befd3cd9d643c8c8979b247a5dacd55b54f8f318
jlmais <joao.brito@lnls.br>
2018-08-27 08:01:36 -0300

    End event receiver section

--- dmtd_analysis.m
+++ dmtd_analysis.m
@@ -1,19 +1,46 @@
 clear all; close all; clc;
 
+z = tf('z');
+
+% RF frequency in Hz
 RF = 500e6;
+% reference clock frequency
 fref = RF*5/36;
+% dmtd parameter and frequency
 N = 144;
 fdmtd = fref*N./(N+1);
 
+% Miminum time difference detectable by dmtd
 tmin = 1./(fref*N);
 
+% sample frequency and sample time of the dmtd
 fbeat = fref./(N+1);
+Ts = 1/fbeat;
 
-fmin = fref-1./(((N+1)/fref+tmin)./(N+1))
+% dmtd phase gain
+Kdmtd = N/(2*pi);
 
-tmin./(1/(fref-1)-1/fref)
+% from FPGA code
+phase_avg = 7;
+phase_div = 1;
 
-1/(N*fref)
+% Moving Average Filter (maf)
+m = phase_avg; 
+d = 2^phase_div;
+num = zeros(1, m+1);
+num(1) = 1;
+num(m+1) = -1;
+den = zeros(1, m+1);
+den(2) = -1;
+den(1) = 1;
+MAF = tf(num, d*den, Ts);
 
-fbeat/(fdmtd*fref)
+% PI controler
+Kp = 10;
+Kvco = 3.3/65536*135/1e6*fref*2*pi;
+
+H = feedback(Kdmtd*MAF*Kp*Kvco*z/(z-1), 1)
+
+abs(pole(H))
+ 
 
